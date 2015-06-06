c5c27911994387680c654ac2299bddf928fec76f
Erin McBride <emcbride750@gmail.com>
2015-06-05 21:49:12 -0700

    imagetransformer interface now handles user configurable parameters

--- Ggoggalggorithm/GenAlgorithmGUI/ImageTransformer.cs
+++ Ggoggalggorithm/GenAlgorithmGUI/ImageTransformer.cs
@@ -37,6 +37,50 @@ namespace GenAlgorithmGUI
         /// <returns></returns>
         public ICollection<System.Windows.Shapes.Polygon> Step(out double Fitness, out int currentStep);
 
+        /// <summary>
+        /// Configurable weights, etc, should be handled with ParameterDelegates.
+        /// </summary>
+        /// <returns></returns>
+        public ICollection<ParameterDelegate> submitParameters();
 
+        public class ParameterDelegate
+        {
+            public string name { get; private set; }
+            public object value { get; set; }
+            public Type type { get; private set; }
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="parameterValue">The value to send to the ImageTransformer instance.</param>
+            /// <returns>True if the value was accepted.</returns>
+            public delegate bool commitParameter(object parameterValue);
+            private commitParameter opToPerform { get; private set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="pName">Name of the parameter. Visible to user in GUI.</param>
+            /// <param name="pValue">Default value of the parameter.</param>
+            /// <param name="pType">Expected type of the parameter. GUI uses this to create the correct control.</param>
+            /// <param name="pOpToPerform">The delegate that attempts to set the value on the ImageTransformer instance. Returns false if the value is outside accepted parameters.</param>
+            public ParameterDelegate(string pName, object pValue, Type pType, commitParameter pOpToPerform)
+            {
+                this.name = pName;
+                this.value = pValue;
+                this.type = pType;
+                this.opToPerform = pOpToPerform;
+            }
+
+            public bool commit(object pValue)
+            {
+                this.value = pValue;
+                return opToPerform(value);
+            }
+
+            public bool commit()
+            {
+                return opToPerform(value);
+            }
+        }
     }
 }

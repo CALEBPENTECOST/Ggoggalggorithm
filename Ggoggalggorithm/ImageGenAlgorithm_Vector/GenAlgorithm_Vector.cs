c53f536e1ff726beab8f6001140968ad1ba00920
Caleb Pentecost <caleb.james.pentecost@gmail.com>
2015-06-05 19:22:48 -0700

    Adding other empty projects/classes

---
+++ Ggoggalggorithm/ImageGenAlgorithm_Vector/GenAlgorithm_Vector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageGenAlgorithm_Vector
+{
+    class GenAlgorithm_Vector
+    {
+        //We want to always keep track of our source image
+
+        //We want to keep track of all our necessary generated images
+
+        //We need a method to load everything
+
+        //We need a method to perform the next step
+
+        //We need a class that represents a child of the algorithm
+
+        public class GenAlg_Vector
+        {
+
+        }
+
+        
+    }
+}

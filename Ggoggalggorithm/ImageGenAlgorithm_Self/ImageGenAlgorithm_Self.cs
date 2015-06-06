7ae3bf98054149c06ae554ece3826c643d634184
Erin McBride <emcbride750@gmail.com>
2015-06-05 22:34:41 -0700

    moved test params into constructor

--- Ggoggalggorithm/ImageGenAlgorithm_Self/ImageGenAlgorithm_Self.cs
+++ Ggoggalggorithm/ImageGenAlgorithm_Self/ImageGenAlgorithm_Self.cs
@@ -17,13 +17,17 @@ namespace ImageGenAlgorithm_Self
 
         LinkedList<ParameterDelegate> testParams;
 
-        public void ImageTransformer(Image BaseImage)
-        {
+        public ImageGenAlgorithm_Self() {
             testParams = new LinkedList<ParameterDelegate>();
 
             testParams.AddFirst(new ParameterDelegate("Test String Parameter", "default value", typeof(string), throwAway));
             testParams.AddFirst(new ParameterDelegate("Test Bool Parameter", false, typeof(bool), throwAway));
             testParams.AddFirst(new ParameterDelegate("Test Integer Parameter", 99, typeof(int), throwAway));
+        }
+
+        public void ImageTransformer(Image BaseImage)
+        {
+            throw new NotImplementedException();
         }
 
         public ICollection<System.Windows.Shapes.Polygon> Step(out double Fitness, out int currentStep)

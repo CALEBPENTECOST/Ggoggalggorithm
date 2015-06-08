fba1032416798018184e26af9bc2d43e2c898a28
Caleb Pentecost <caleb.james.pentecost@gmail.com>
2015-06-07 21:48:17 -0700

    Adding beginning of vector algorithm

--- Ggoggalggorithm/ImageGenAlgorithm_Vector/GenAlgorithm_Vector.cs
+++ Ggoggalggorithm/ImageGenAlgorithm_Vector/GenAlgorithm_Vector.cs
@@ -3,26 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows;
 
 namespace ImageGenAlgorithm_Vector
 {
-    class GenAlgorithm_Vector
+    class GenAlgorithm_Vector : ImageGenAlgorithmLib.ImageTransformer
     {
-        //We want to always keep track of our source image
 
-        //We want to keep track of all our necessary generated images
 
-        //We need a method to load everything
 
-        //We need a method to perform the next step
-
-        //We need a class that represents a child of the algorithm
-
-        public class GenAlg_Vector
+        public class VectorPolygon
         {
+            //A vector polygon needs to 
+            public VectorPolygon(GenAlgorithm_Vector owner)
+            {
+                _owner = owner;
+            }
+            //A vector polygon has an origin point
+            int origin_x, origin_y;
 
+            //A vector polygon has a list vector magnitues
+            List<double> vectorMagnitudes;
+
+            //A vetor polygon has a list of vector angles (as a percent of 0 to 2pi)
+            List<double> vectorAnglePercent;
+
+            //A vector polygon needs a way to determine all of its indices
+
+            //A vector polygon needs to know how to reproduce
+
+            //A vector polygon needs to know how to mutate
+
+            internal GenAlgorithm_Vector _owner { get; set; }
         }
 
-        
+
+
+        public void LoadImage(Image BaseImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ImageGenAlgorithmLib.Polygon> Step(out double fitness, out int currentStep)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<ImageGenAlgorithmLib.ParameterDelegate> getParameters()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
